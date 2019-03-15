324f7d9a693fa0e61c236f01099f6be07195247b
petems <p.morsou@gmail.com>
2019-03-15 22:40:32 +0000

    gitsweeper cleanup

--- main.go
+++ main.go
@@ -18,7 +18,13 @@ import (
 )
 
 var (
-	debug = kingpin.Flag("debug", "Enable debug mode.").Bool()
+	app = kingpin.New("gitsweeper", "A command-line tool for cleaning up merged branches.")
+
+	debug = app.Flag("debug", "Enable debug mode.").Bool()
+
+	preview = app.Command("preview", "Show the branches to delete.")
+
+	cleanup = app.Command("cleanup", "Delete the remote branches.")
 )
 
 func setupLogger() {
@@ -52,7 +58,6 @@ func remoteBranches(s storer.ReferenceStorer) (storer.ReferenceIter, error) {
 
 func main() {
 	kingpin.Version("0.0.1")
-	kingpin.Parse()
 
 	setupLogger()
 
@@ -61,86 +66,92 @@ func main() {
 		log.Info("--debug setting detected - Info level logs enabled")
 	}
 
-	log.Info("Getting current working directory")
+	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
+	case preview.FullCommand():
+		log.Info("Getting current working directory")
 
-	dir, err := os.Getwd()
+		dir, err := os.Getwd()
 
-	log.Infof("Current working directory is %s", dir)
+		log.Infof("Current working directory is %s", dir)
 
-	if err != nil {
-		fatalError("Error opening current directory:", err)
-		return
-	}
+		if err != nil {
+			fatalError("Error opening current directory:", err)
+			return
+		}
 
-	log.Infof("Attempting to open Git drectory at %s", dir)
+		log.Infof("Attempting to open Git drectory at %s", dir)
 
-	repo, err := git.PlainOpen(dir)
+		repo, err := git.PlainOpen(dir)
 
-	if err != nil {
-		fatalError(fmt.Sprintf("Error reading %s as Git repo", dir), err)
-		return
-	}
+		if err != nil {
+			fatalError(fmt.Sprintf("Error reading %s as Git repo", dir), err)
+			return
+		}
 
-	log.Info("Attempting to get master information from branches from repo")
+		log.Info("Attempting to get master information from branches from repo")
 
-	branchRefs, err := repo.Branches()
-	if err != nil {
-		fatalError("list branches failed:", err)
-	}
+		branchRefs, err := repo.Branches()
+		if err != nil {
+			fatalError("list branches failed:", err)
+		}
 
-	branchHeads := make(map[string]plumbing.Hash)
+		branchHeads := make(map[string]plumbing.Hash)
 
-	fmt.Println("Fetching from the remote...")
+		fmt.Println("Fetching from the remote...")
 
-	err = branchRefs.ForEach(func(reference *plumbing.Reference) error {
-		branchName := strings.TrimPrefix(reference.Name().String(), "refs/heads/")
-		branchHead := reference.Hash()
-		branchHeads[branchName] = branchHead
-		return nil
-	})
+		err = branchRefs.ForEach(func(reference *plumbing.Reference) error {
+			branchName := strings.TrimPrefix(reference.Name().String(), "refs/heads/")
+			branchHead := reference.Hash()
+			branchHeads[branchName] = branchHead
+			return nil
+		})
 
-	if err != nil {
-		fatalError("list branches failed:", err)
-		return
-	}
+		if err != nil {
+			fatalError("list branches failed:", err)
+			return
+		}
 
-	masterCommits, err := repo.Log(&git.LogOptions{From: branchHeads["master"]})
+		masterCommits, err := repo.Log(&git.LogOptions{From: branchHeads["master"]})
 
-	remoteBranches, err := remoteBranches(repo.Storer)
+		remoteBranches, err := remoteBranches(repo.Storer)
 
-	remoteBranchHeads := make(map[string]plumbing.Hash)
+		remoteBranchHeads := make(map[string]plumbing.Hash)
 
-	err = remoteBranches.ForEach(func(branch *plumbing.Reference) error {
-		remoteBranchName := strings.TrimPrefix(branch.Name().String(), "refs/remotes/")
-		remoteBranchHead := branch.Hash()
-		remoteBranchHeads[remoteBranchName] = remoteBranchHead
-		return nil
-	})
+		err = remoteBranches.ForEach(func(branch *plumbing.Reference) error {
+			remoteBranchName := strings.TrimPrefix(branch.Name().String(), "refs/remotes/")
+			remoteBranchHead := branch.Hash()
+			remoteBranchHeads[remoteBranchName] = remoteBranchHead
+			return nil
+		})
 
-	for branchName, branchHead := range remoteBranchHeads {
-		log.Infof("Remote Branch %s head is: %s", branchName, branchHead)
-	}
-
-	mergedBranches := make([]string, 0)
-
-	err = masterCommits.ForEach(func(commit *object.Commit) error {
 		for branchName, branchHead := range remoteBranchHeads {
-			if (branchHead.String() == commit.Hash.String()) && (branchName != "origin/master") {
-				log.Infof("Branch %s head (%s) was found in master, so has been merged!\n", branchName, branchHead)
-				mergedBranches = append(mergedBranches, branchName)
+			log.Infof("Remote Branch %s head is: %s", branchName, branchHead)
+		}
+
+		mergedBranches := make([]string, 0)
+
+		err = masterCommits.ForEach(func(commit *object.Commit) error {
+			for branchName, branchHead := range remoteBranchHeads {
+				if (branchHead.String() == commit.Hash.String()) && (branchName != "origin/master") {
+					log.Infof("Branch %s head (%s) was found in master, so has been merged!\n", branchName, branchHead)
+					mergedBranches = append(mergedBranches, branchName)
+				}
+			}
+			return nil
+		})
+
+		if len(mergedBranches) == 0 {
+			fmt.Println("No branches already merged into master!")
+		} else {
+			sort.Strings(mergedBranches)
+			fmt.Println("\nThese branches have been merged into master:")
+			for _, branchName := range mergedBranches {
+				fmt.Printf("  %s\n", branchName)
 			}
 		}
-		return nil
-	})
-
-	if len(mergedBranches) == 0 {
-		fmt.Println("No branches already merged into master!")
-	} else {
-		sort.Strings(mergedBranches)
-		fmt.Println("\nThese branches have been merged into master:")
-		for _, branchName := range mergedBranches {
-			fmt.Printf("  %s\n", branchName)
-		}
+	case cleanup.FullCommand():
+		fmt.Println("Cleanup has not been implemented yet!")
+		os.Exit(1)
 	}
 
 }
